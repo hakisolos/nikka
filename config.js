143f668e85207235a9918083f2c4ff871bb2be71
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-09-17 10:26:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ function toBool(value, defaultValue = false) {
 
 export default {
     OWNER: process.env.OWNER || "2349112171078",
-    IMG: process.env.IMG || "http://cdn-haki.zone.id/files/H4Ouln.jpg",
+    IMG: process.env.IMG || "http://files.haki.top/files/zQypx4.jpg",
     BOT: process.env.BOT || "994401499031",
     LOGGER: toBool(process.env.LOGGER, true),
     MODS: (process.env.MODS || "2349112171078,113439162822839").split(","),
     PREFIX: process.env.PREFIX || "?",
-    SESSION_ID: process.env.SESSION_ID || "NIKKA-2025-90-14-Hpm4OJ",
+    SESSION_ID: process.env.SESSION_ID || "NIKKA-2025-09-17-TUGSWq",
     MODE: process.env.MODE || "private"
 };
