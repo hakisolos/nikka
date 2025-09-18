efcee0d984899d9e0b6e43c52f3a2a316bf75166
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-09-18 16:10:04 +0100

    Create user.js

---
+++ plugins/user.js
@@ -0,0 +1,25 @@
+import {command,isPrivate} from "./commands.js"
+
+command(
+  {
+    name: "getpp",
+    desc: "Get profile picture",
+    fromMe: isPrivate,
+    react: true,
+    type: "utility",
+  },
+  async (msg) => {
+    let target = msg.quoted?.sender || msg.jid;
+
+    try {
+      const ppUrl = await msg.client.profilePictureUrl(target, "image");
+      await msg.client.sendMessage(
+        msg.jid,
+        { image: { url: ppUrl } },
+        { quoted: msg.raw }
+      );
+    } catch {
+      await msg.reply("No profile picture found.");
+    }
+  }
+);
