8fa4b8e143128e8754263237d47dff9158ea42d6
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-09-18 14:49:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+import { Sticker, StickerTypes } from "wa-sticker-formatter";
+import config from "../config.js"
+export async function toSticker(msg, buffer) {
+  try {
+    const sticker = new Sticker(buffer, {
+      pack: config.stickerPack,
+      type: StickerTypes.FULL,
+      categories: ["🤩", "🎉"],
+      id: "12345",
+      quality: 70,
+    });
+
+    const stickerBuffer = await sticker.toBuffer();
+
+    await msg.client.sendMessage(
+      msg.jid,
+      { sticker: stickerBuffer },
+      { quoted: msg.raw }
+    );
+  } catch (err) {
+    console.error("Sticker creation failed:", err);
+    await msg.reply("Something went wrong >_<");
+  }
+}
 export function isAdmin(metadata, userId) {
   if (!metadata || !metadata.participants || !userId) return false;
 
